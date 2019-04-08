25f45738de465d4a6f149b3b81f0655d43d6e6a1
AkmatovEldi <akmatoveldi@gmail.com>
2019-04-08 17:10:51 +0600

    ex21 and ex20 Done (Function and Files, Function can Return)

---
+++ exercise/ex21.py
@@ -0,0 +1,50 @@
+def add(a, b):
+    print(f"ADDING {a} + {b}")
+    return a + b
+
+
+def substract(a, b):
+    print(f"Subtracting {a} - {b}")
+    return a - b
+
+
+def multiply(a, b):
+    print(f"Multiplying {a} * {b}")
+    return a * b
+
+
+def divide(a, b):
+    print(f"Dividing {a} / {b}")
+    return a / b
+
+
+print("Let's do some math with just functions!")
+
+age = add(30, 5)
+height = substract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
+
+# A puzzle for the extra credit, type it in anyway.
+print("Here is a puzzle.")
+
+what = add(age, substract(height, multiply(weight, divide(iq, 2))))
+
+print("That becomes: ", what, "Can you do it by hand?")
+
+
+print("My Function")
+
+
+def add_multiply(a, b):
+    print(f"Сначало прибавляем {a} + {b}, а потом умножаем на {b}")
+    age = a + b
+    return age * b
+
+
+trushno = add_multiply(10, 2)
+print(f"Сумма {trushno}")
+
+
