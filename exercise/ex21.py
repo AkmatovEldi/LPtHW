fd5211b6d5c718b56a877bd30287676903efb5cd
AkmatovEldi <akmatoveldi@gmail.com>
2019-04-08 17:18:36 +0600

    modified ex21.py

--- exercise/ex21.py
+++ exercise/ex21.py
@@ -17,6 +17,10 @@ def divide(a, b):
     print(f"Dividing {a} / {b}")
     return a / b
 
+def add_multiply(a, b):
+    print(f"Сначало прибавляем {a} + {b}, а потом умножаем на {b}")
+    age = a + b
+    return age * b
 
 print("Let's do some math with just functions!")
 
@@ -24,27 +28,20 @@ age = add(30, 5)
 height = substract(78, 4)
 weight = multiply(90, 2)
 iq = divide(100, 2)
+trushno = add_multiply(10, 2)
 
 print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
 
 # A puzzle for the extra credit, type it in anyway.
 print("Here is a puzzle.")
 
-what = add(age, substract(height, multiply(weight, divide(iq, 2))))
+what = add(age, substract(height, multiply(weight, add_multiply(trushno, divide(iq, 2)))))
 
 print("That becomes: ", what, "Can you do it by hand?")
 
 
 print("My Function")
 
-
-def add_multiply(a, b):
-    print(f"Сначало прибавляем {a} + {b}, а потом умножаем на {b}")
-    age = a + b
-    return age * b
-
-
-trushno = add_multiply(10, 2)
 print(f"Сумма {trushno}")
 
 
